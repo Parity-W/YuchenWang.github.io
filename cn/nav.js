62a8fa238ace7a5a5e18594ba435763ae475a63e
Yuchen <111476469+Parity-W@users.noreply.github.com>
2023-05-27 18:55:44 +0800

    Update nav.js

--- cn/nav.js
+++ cn/nav.js
@@ -1,10 +1,9 @@
 document.writeln("		<nav>");
 document.writeln("    		<ul>");
-document.writeln("        		<li><a href=\"../cn/index.html\">主页</a></li>");
-document.writeln("        		<li><a href=\"talks.html\">学术报告</a></li>");
-document.writeln("        		<li><a href=\"teaching.html\">教学</a></li>");
-document.writeln("        		<li><a href=\"doc.html\">文档</a></li>");
-// document.writeln("        		<li><a href=\"starcharts.html\">星图</a></li>");
-document.writeln("        		<li><a href=\"links.html\">链接</a></li>");
+document.writeln("        		<li><a href=\"../cn/index.html\">角色</a></li>");
+document.writeln("        		<li><a href=\"doc.html\">装备</a></li>");
+document.writeln("        		<li><a href=\"scholar.html\">法宝</a></li>");
+document.writeln("        		<li><a href=\"life.html\">宠物</a></li>");
+document.writeln("        		<li><a href=\"share.html\">客栈</a></li>");
 document.writeln("    		</ul>");
 document.writeln("		</nav>");
